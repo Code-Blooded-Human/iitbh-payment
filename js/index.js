4439cb446526e3061db51bef076686c2cdd69d10
unknown <abhisheks@iitbhilai.ac.in>
2019-01-24 20:40:11 +0530

    ISODEP

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 const DESFIRE_SELECT_PICC = '00 A4 00 04 02 3F 00 20';
 
-
-
+function test(){
+  console.log("Connected");
+}
+function nottest(){
+  console.log("Not Connected");
+}
  function handleDesfire(nfcEvent) {
 
     const tagId = nfc.bytesToHexString(nfcEvent.tag.id);
     console.log('Processing', tagId);
 
     try {
-        nfc.connect(function(){
-          console.log("Connected");
-        });
+        nfc.connect(test,nottest);
 
         //response =  nfc.transceive(DESFIRE_SELECT_AID);
 
