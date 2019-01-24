f2adec7caea7597abddf19e13f3329a0bc5499cf
unknown <abhisheks@iitbhilai.ac.in>
2019-01-24 09:43:13 +0530

    test2

--- js/index.js
+++ js/index.js
@@ -33,8 +33,31 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
+    app.receivedEvent('deviceready');
+
+    // Read NDEF formatted NFC Tags
+    nfc.addNdefListener (
+        function (nfcEvent) {
+            var tag = nfcEvent.tag,
+                ndefMessage = tag.ndefMessage;
+
+            // dump the raw json of the message
+            // note: real code will need to decode
+            // the payload from each record
+            alert(JSON.stringify(ndefMessage));
+
+            // assuming the first record in the message has
+            // a payload that can be converted to a string.
+            alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+        },
+        function () { // success callback
+            alert("Waiting for NDEF tag");
+        },
+        function (error) { // error callback
+            alert("Error adding NDEF listener " + JSON.stringify(error));
+        }
+    );
+},
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
